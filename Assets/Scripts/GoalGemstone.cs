60308a10924e09d657963f1aeb48ae6d40817409
ZidaneDC <azyzadc@gmail.com>
2020-04-13 11:06:27 -0700

    Finished Project

---
+++ Assets/Scripts/GoalGemstone.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoalGemstone : MonoBehaviour
+{
+    public AudioClip pickupClip;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (LayerMask.LayerToName(collision.gameObject.layer) == "Player")
+        {
+            AudioSource.PlayClipAtPoint(pickupClip, transform.position);
+            Destroy(this.gameObject);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
